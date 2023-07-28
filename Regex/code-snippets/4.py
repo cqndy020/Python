1321ee0aaa339e4b9cc8b6b0c1ec9578169309ed
Kelvin <w7.lwinho.au@gmail.com>
2023-07-28 16:38:30 +0630

    update code

--- Regex/code-snippets/4.py
+++ Regex/code-snippets/4.py
@@ -1,7 +1,7 @@
 import re
 
 ph_no = '+(95)987-654-321'
-match = re.findall('\D', ph_no)
+match = re.findall(r'\D', ph_no)
 
 for x in match:
     print(x)
@@ -9,6 +9,12 @@ for x in match:
 
 
 #using sub() function
-result = re.sub('\D', '', ph_no)
+result = re.sub(r'\D', '', ph_no)
 print(result)
 #output: 95987654321
+
+#using word boundary
+s = 'I eat three meals in a day: breakfast, lunch, and dinner.'
+match = re.search(r'\blunch\b', s)
+print(match.group())
+#output: lunch
