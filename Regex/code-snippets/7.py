d3fe2df7a850a6316966f6c6ff0e1f77e0ba54a2
Kelvin <w7.lwinho.au@gmail.com>
2023-08-02 16:38:52 +0630

    update code

--- Regex/code-snippets/7.py
+++ Regex/code-snippets/7.py
@@ -10,3 +10,10 @@ x = "I am 5 feet and 11 inches tall."
 matches = re.finditer(r"\d+(?=\s*feet)", x)
 for match in matches:
     print(match.group())
+    #output: 5
+
+y = "I love apple pie, but not apple sauce."
+matches = re.findall(r"\w+(?=[,.])", y)
+for match in matches:
+    print(match)
+    #output: pie\nsauce
