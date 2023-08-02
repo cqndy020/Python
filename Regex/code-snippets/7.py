c407558f9069ed5fd2bcfd44c5cfd96023e5807d
Kelvin <w7.lwinho.au@gmail.com>
2023-08-02 15:57:24 +0630

    update code

--- Regex/code-snippets/7.py
+++ Regex/code-snippets/7.py
@@ -4,3 +4,9 @@ s = "Python Python is easy"
 #\1 references the (\w+) capturing group
 new_s = re.sub(r"(\w+)\s+\1", r"\1", s)
 print(new_s)
+
+#using lookahead syntax->>  ?=
+x = "I am 5 feet and 11 inches tall."
+matches = re.finditer(r"\d+(?=\s*feet)", x)
+for match in matches:
+    print(match.group())
