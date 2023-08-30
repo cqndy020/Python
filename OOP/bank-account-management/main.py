2d71321cb5511c1d594d91c9111543dae7bfbcd2
Kelvin Jinn <w7.lwinho.au@gmail.com>
2023-08-30 23:58:13 +0630

    added type annotations and updated some lines

--- OOP/bank-account-management/main.py
+++ OOP/bank-account-management/main.py
@@ -7,7 +7,7 @@ Account type: Savings / Checking
 """
 
 class Account:
-    def __init__(self, account_number: float, account_holder, type):
+    def __init__(self, account_number: float, account_holder: str, type: str):
         self.account_number = account_number
         self.account_holder = account_holder
         self.balance = 0
@@ -18,7 +18,7 @@ class Account:
         return f"Your balance: {self.balance}."
     
     def display_account_info(self):
-        return f"Account Number: {self.account_number}\nAccount Holder: {self.account_holder}\nBalance: {self.balance}\nAccount Type: {self.type}"
+        return f"\nAccount Number: {self.account_number}\nAccount Holder: {self.account_holder}\nBalance: {self.balance}\nAccount Type: {self.type}"
     
     def deposit(self, amount:float):
         assert amount >= 0, 'The amount should be positive'
@@ -36,15 +36,11 @@ class Account:
             print(f'You can only withdraw: ${self.balance}')
 
 class Bank:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
         self.accounts = []
 
-    def __str__(self):
-        if Bank:
-            print('New transaction occurs.')
-
-    def create_account(self, account_number, account_holder, type):
+    def create_account(self, account_number: int, account_holder: str, type: str):
         account = Account(account_number, account_holder, type)
         self.accounts.append(account)
         return account
@@ -65,25 +61,28 @@ class Bank:
         elif transaction_type == 'Withdrawal':
             account.withdraw(amount)
             print('Withdraw successfully!')
-            
+
         else:
             print("Transaction could only deposit and withdraw.\nTo deposit: Type 'Deposit'\nTo Withdraw: Type 'Withdrawal'")
 
 class Transaction:
-    def __init__(self, transaction_type, amount:float):
-        assert amount >= 0, "The amount should be more than or equal to 0."
-
+    def __init__(self, transaction_type: str, amount:float):
         self.transaction_type = transaction_type
         self.amount = amount
 
     def display_transaction(self):
         return f"{self.transaction_type}  ${self.amount}"
 
+#create bank name
 bank = Bank('KBZ')
 
+#create accounts
 acc1 = bank.create_account('12345', 'Alice', 'Savings')
 acc2 = bank.create_account('56789', 'Bob', 'Checking')
 
-acc1.deposit(100.0)
+#deposit from account and withdraw from bank
+# acc1.deposit(100.0)
+# bank.perform_transaction(acc1, 'Withdrawal', 55)
 
-bank.perform_transaction(acc1, 'withdrawal', 55)
+#display accounts
+bank.display_accounts()
